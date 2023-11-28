b43811bd44cbfa6ac0f0a9eabe95975acd37d823
IceGawd <avighna.kukreja@gmail.com>
2023-11-28 08:30:17 -0800

    now with percentage

--- grade.sh
+++ grade.sh
@@ -1,3 +1,4 @@
+# CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -8,9 +9,75 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ ! -f student-submission/ListExamples.java ]]
+then
+	echo 'ListExamples.java does not exist!'
+	exit
+fi
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+cp student-submission/ListExamples.java grading-area/ListExamples.java
+cp TestListExamples.java grading-area/TestListExamples.java
+cp -r lib grading-area/lib
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cd grading-area
+
+echo "Compiling..."
+javac -cp $CPATH *.java
+echo "Compiling Finished!"
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junitOutput.txt
+OUTPUT=`grep "Tests run" junitOutput.txt`
+
+# cat junitOutput.txt
+# echo $OUTPUT
+
+OUTPUT=$OUTPUT","
+
+PHASE=1
+FAILURES=0
+TESTS=0
+LENGTH=${#OUTPUT}
+TEMPSTRING=""
+for (( c=0; c<LENGTH; c++ ))
+do
+	CHAR=${OUTPUT:$c:1}
+	# echo $CHAR
+	if [[ $PHASE -eq 1 || $PHASE -eq 3 ]]
+	then
+		# Finding Semicolon for Tests Run
+		if [[ $CHAR == ":" ]]
+		then
+			PHASE=$(($PHASE+1))
+			TEMPSTRING=""
+		fi
+	elif [[ $PHASE -eq 2 || $PHASE -eq 4 ]] then
+		# Getting Tests Run
+		if [[ $CHAR == "," ]]
+		then
+			if [[ $PHASE -eq 2 ]]
+			then
+				TESTS=${TEMPSTRING:1}
+			else
+				FAILURES=${TEMPSTRING:1}
+			fi
+			PHASE=$(($PHASE+1))
+		fi
+		TEMPSTRING=$TEMPSTRING$CHAR
+	fi
+done
+
+OKOUTPUT=`grep "OK" junitOutput.txt`
+if [[ ${#OKOUTPUT} != 0 ]]
+then
+	OKOUTPUT=${OKOUTPUT#*\(}
+	OKOUTPUT=${OKOUTPUT%[ ]*}
+	TESTS=$OKOUTPUT
+fi
+
+# echo "PHASE: $PHASE"
+# echo "FAILURES: $FAILURES"
+# echo "TESTS: $TESTS"
+PASSING=$(($TESTS-$FAILURES))
+PERCENT=$(((100*$PASSING)/$TESTS))
+echo "GRADE: $PERCENT% ($PASSING / $TESTS)"
+
+cd ..
